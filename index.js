cdc0d3c53e62b9bd28116d0bca64d99429d91035
Katherine Jean-Charles <katherinejc103@gmail.com>
2021-12-11 13:25:08 -0500

    Done

---
+++ index.js
@@ -0,0 +1,30 @@
+describe("shout(string)", function () {
+    it("receives one argument and returns it in all caps", function () {
+      expect(shout("hello")).toEqual("HELLO");
+    });
+  });
+  function shout(string) {}
+  function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {}
+  function whisper(string) {
+      return string.toLowerCase();
+  }
+  function logShout(string) {}
+  function logShout(string) {
+      return string.toUpperCase();
+  }
+  function logwhisper(string) {}
+  function logwhisper(string) {
+      return string.toLowerCase();
+  }
+  function sayHiToGrandma(string) {}
+  function sayHiToGrandma(string) {
+      return "I love you, too." ();
+      if(string) 'is' "I love you, Grandma";
+  }
